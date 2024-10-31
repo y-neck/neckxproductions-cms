0f7f3181ecd4034e35066b533552524ec85a8bae
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-10-31 20:28:28 +0100

    install tailwind & tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,68 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+    "./storyblok/**/*.vue",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      minSize: '120px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      primary: '#eee',
+      background: '#1f1f1f',
+      secondary: '#535353',
+      accent: '#2997FF',
+      errorRed: '#B5235F',
+    },
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+    },
+
   },
-  plugins: [],
-}
+  extend: {	/* Extend default css values */
+    fontSize: {
+      'p': ['1rem', {
+        lineHeight: '1.5rem',
+        letterSpacing: '0rem',
+        fontWeight: '400',
+      }],
+      'h3': ['1.4rem', {
+        lineHeight: 'auto',
+        letterSpacing: '0rem',
+        fontWeight: '400',
+      }],
+      'h2': ['1.75rem', {
+        lineHeight: 'auto',
+        letterSpacing: '0rem',
+        fontWeight: '400',
+      }],
+      'h1': ['2rem', {
+        lineHeight: 'auto',
+        letterSpacing: '0rem',
+        fontWeight: '400',
+      }],
+      'sectionTitle': ['2rem', {
+        lineHeight: 'auto',
+        letterSpacing: '0.04rem',
+        fontWeight: '700',
+        fontStyle: 'italic',
+      }]
+    },
+    prefix: 'tw-', /* PostCSS: Add prefix for tailwind classes */
+  },
+  plugins: [require('@tailwindcss/typography')],
+  corePlugins: {
+    preflight: true,	/* PreCSS: reset default stylings for html elements to be defined by custom styles */
+  }
+};
 
