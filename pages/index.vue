0f84a7687e8c4fc40a9031dd84316f475426a4ff
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-11-11 22:03:25 +0100

    /Header.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-    <!-- Import first component -->
-    <Hero v-if="story" :blok="story.content" />
     <!-- <pre>{{ story }}</pre> -->
-    <h1>Home</h1>
+    <!-- Import first component -->
+    <Hero v-if="story" :blok="story.content" class="block sticky top-0 z-1" />
+    <div
+      id="header-section"
+      class="translate-y-[-72px] max-md:translate-y-[-32px] z-10"
+    >
+      <div
+        id="header-xIcon-Container"
+        class="flex justify-center items-center translate-y-[32px] max-md:translate-y-[0px]"
+      >
+        <img
+          id="header-xIcon"
+          role="img"
+          aria-label="xIcon"
+          src="https://a.storyblok.com/f/310287/1850x1668/d7b1985a56/x-icon.webp/m/"
+          alt="x-icon, dekorativ"
+          class="w-[6rem] max-md:w-[4rem] self-center pointer-events-none"
+        />
+      </div>
+      <Header />
+    </div>
+    <StoryblokComponent v-if="story" :blok="story.content" class="z-10" />
   </div>
 </template>
 
 <script setup lang="ts">
-import Hero from '~/Storyblok/components/Hero.vue';
-import Header from '~/components/Header.vue';
+import Hero from '~/storyblok/components/Hero.vue';
+import Header from '~/storyblok/globals/Header.vue';
 
 const config = useRuntimeConfig();
 const story = await useAsyncStoryblok('home', {
