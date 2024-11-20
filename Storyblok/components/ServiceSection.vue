824404d8d9977f187dd737f339db6e59dbc80e71
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-11-20 11:07:59 +0100

    remove redundant code

--- Storyblok/components/ServiceSection.vue
+++ Storyblok/components/ServiceSection.vue
@@ -11,7 +11,6 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
 import Service from '~/components/Service.vue';
 
 // accept blok as prop
@@ -19,45 +18,6 @@ const props = defineProps({
   blok: { required: true, type: Object },
 });
 
-// const config = useRuntimeConfig();
-// const services = ref([]); // This will hold the actual service objects
-
-// // Resolve the service references (servicesRef) into actual service data
-// onMounted(async () => {
-//   const story = await useAsyncStoryblok('home', {
-//     version: config.public.storyblok_version,
-//   });
-
-//   // Check if story is successfully retrieved
-//   console.log('Home story:', story.value);
-
-//   const serviceSection = story.value?.content?.body.find(
-//     (blok) => blok.component === 'serviceSection'
-//   );
-
-//   if (serviceSection) {
-//     const servicePromises = serviceSection.servicesRef.map(async (uuid) => {
-//       try {
-//         const serviceStory = await useAsyncStoryblok(`service/${uuid}`, {
-//           version: config.public.storyblok_version,
-//         });
-//         return serviceStory.value?.content || undefined;
-//       } catch (error) {
-//         console.error(`Failed to fetch service with UUID ${uuid}:`, error);
-//         return undefined;
-//       }
-//     });
-
-//     const resolvedServices = await Promise.all(servicePromises);
-//     services.value = resolvedServices.filter(
-//       (service) => service !== undefined
-//     );
-
-//     console.log('Resolved services:', services.value);
-//   } else {
-//     console.warn('No serviceSection found in the home story content.');
-//   }
-// });
 </script>
 
 <style scoped></style>
