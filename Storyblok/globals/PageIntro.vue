69862582394f361fa3d813c4295d948af1e2604a
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-11-20 11:09:52 +0100

    /PageIntro.vue

---
+++ Storyblok/globals/PageIntro.vue
@@ -0,0 +1,38 @@
+<template>
+  <section
+    v-editable="blok"
+    class="flex flex-col md:flex-row gap-8"
+  >
+    <div
+      class="about-img-container overflow-hidden skew-x-[-30deg] mx-[-60px] min-w-[40%]"
+    >
+      <img
+        id="about-img"
+        class="skew-x-[30deg] w-full h-full object-cover relative object-left-top overflow-hidden min-w-[40%]"
+        v-if="sectionImage"
+        :src="`${sectionImage?.filename}/m/smart`"
+        :alt="sectionImage.alt"
+      />
+    </div>
+    <div class="intro-text min-w-[40%] flex flex-col gap-8 md:ml-16">
+      <span id="intro-title" class="text-md">{{ introTitle }}</span>
+      <p id="intro-text" class="">{{ introText }}</p>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  blok: { type: Object },
+});
+
+// Access claimText and claimOutline from props.blok
+const sectionImage = props.blok?.sectionImage;
+const introTitle = props.blok?.introTitle;
+const introText = props.blok?.introText;
+
+// Debugging logs
+console.log('PageIntro.vue: ', props.blok);
+</script>
+
+<style scoped></style>
---
+++ pages/about.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <!-- <pre>{{ story }}</pre> -->
+    <Header />
+    <main>
+      <StoryblokComponent v-if="story" :blok="story.content" />
+
+      <ProjectSectionGrid  />
+
+      <section id="about-vision">
+        <h2 id="about-vision-title" class="sectionTitle">
+          <span class="sectionTitle_outline">Meine </span>
+          <span>Vision</span>
+        </h2>
+        <p id="about-vision-text">
+          Film ist eine der emotionalsten Möglichkeiten, Geschichten zu erzählen
+          – Geschichten von Menschen, von Projekten, vom Leben… <br />Seit
+          meiner Kindheit interessiere ich mich für Kameras, Fotografie und
+          Videoproduktion. Waren es früher noch kurze Videos mit einer billigen
+          Digitalkamera, kam mit den Jahren immer mehr Passion dazu und ich habe
+          mein Wissen und Können in diesen Bereichen, nicht zuletzt durch das
+          Multimedia-Production-Studium, konstant erweitert. <br /><br />
+
+          Gegen Ende meiner Jugend kam das Interesse für das Programmieren und
+          Webentwicklung dazu. Eine Website ist in der heutigen Zeit der eigene
+          gesellschaftliche Auftritt, eine Möglichkeit, sich zu präsentieren und
+          anderen zu zeigen, was man tut und kann. <br />
+          Mich fasziniert, wie aus wenigen Zeilen Code funktionsfähige Webapps
+          entstehen können. <br /><br />
+
+          Diese Fähigkeiten und Mittel einzusetzen, um Geschichten zu erzählen
+          und anderen Menschen zu helfen, ihre Geschichten und Projekte zum
+          Leben zu erwecken, ist meine grosse Leidenschaft.
+        </p>
+      </section>
+      <Footer />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from '~/components/Header.vue';
+import ProjectSectionGrid from '~/storyblok/components/ProjectSectionGrid.vue';
+import Footer from '~/components/Footer.vue';
+
+const config = useRuntimeConfig();
+const story = await useAsyncStoryblok('about', {
+  version: config.public.storyblok_version,
+  resolve_relations: 'ProjectSection.projectRef',
+}); /* Fetch data from Storyblok */
+console.log(story.value);
+</script>
+
+<style scoped></style>
