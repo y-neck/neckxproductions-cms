e30a64852b6869c2fbe0f238c7573f7b41fbfd62
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-11-22 14:37:09 +0100

    /ProjectCard adjust link structure

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,22 +1,24 @@
 <template>
   <!-- <pre>{{ blok }}</pre> -->
-  <div class="project-card-container aspect-video">
-    <div
-      class="project-card card-tile h-full cursor-pointer relative overflow-scroll bg-accent border-2 border-primary rounded-lg"
-    >
-      <img
-        :src="`${blok?.content.projectImg?.filename}/m/smart`"
-        :alt="`${blok?.content.projectImg?.alt}`"
-        loading="lazy"
-        class="project-img w-full h-full aspect-video object-cover z-[-10]"
-      />
-      <NuxtLink
-        :to="`${blok?.content.slug}`"
-        class="sectionTitle z-10 text-sm font-semibold text-center max-h-fit p-6 absolute bottom-0 left-0 w-full bg-background bg-opacity-50 text-decoration-none hover:underline decoration-accent"
-        >{{ blok?.content.projectTitle }}</NuxtLink
+  <NuxtLink :to="`${blok?.content.slug}`">
+    <div class="project-card-container aspect-video">
+      <div
+        class="project-card card-tile h-full cursor-pointer relative overflow-scroll bg-accent border-2 border-primary rounded-lg"
       >
-    </div>
-  </div>
+        <img
+          :src="`${blok?.content.projectImg?.filename}/m/smart`"
+          :alt="`${blok?.content.projectImg?.alt}`"
+          loading="lazy"
+          class="project-img w-full h-full aspect-video object-cover z-[-10]"
+        />
+        <div
+          class="sectionTitle z-10 text-sm font-semibold text-center max-h-fit p-6 absolute bottom-0 left-0 w-full bg-background bg-opacity-50 text-decoration-none hover:underline decoration-accent"
+        >
+          {{ blok?.content.projectTitle }}
+        </div>
+      </div>
+    </div></NuxtLink
+  >
 </template>
 
 <script setup lang="ts">
