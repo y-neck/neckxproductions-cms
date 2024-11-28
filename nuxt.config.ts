a097247be0de76fe127ab2625c0c0b966c666786
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2024-11-28 14:46:33 +0100

    analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,12 +39,42 @@ export default defineNuxtConfig({
       link: [
         {
           rel: 'canonical',
-          href: 'https://neckXproductions.ch',
+          href: 'https://neckxproductions.ch',
         },
       ],
       htmlAttrs: {
         lang: 'de' /* Set lang attribute on <html> tag */,
       },
+      script: [
+        // Matomo Tracking Code
+        {
+          hid: 'matomo', // Unique identifier for the script
+          innerHTML: `
+            var _paq = window._paq = window._paq || [];
+            _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+            _paq.push(["setCookieDomain", "*.neckxproductions.ch"]);
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//analytix.neckxproductions.ch/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+          `,
+          type: 'text/javascript',
+        },
+        {
+          hid: 'matomo-noscript',
+          innerHTML: `
+            <noscript>
+              <p><img referrerpolicy="no-referrer-when-downgrade" src="//analytix.neckxproductions.ch/matomo.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p>
+            </noscript>
+          `,
+          type: 'text/html',
+        },
+      ],
     },
   },
 });
